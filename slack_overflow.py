940a2fb5a77152163fb0ecea75d6659208d183bc
Kyle Lui <klui@godaddy.com>
2021-07-20 16:33:57 -0400

    channel fix

--- slack_overflow.py
+++ slack_overflow.py
@@ -1,8 +1,13 @@
 from flask import Flask
 import os
+import logging
 from joblib import dump, load
 from flask import request, jsonify, abort
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
+client = WebClient(token=os.environ.get("xoxb-2303935478769-2297880316244-vKtkyqSkDMB6Ek6qeIGEJWtl"))
+logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 @app.route('/where', methods=['POST'])
@@ -16,8 +21,31 @@ def recommend():
         return "Please enter a question."
 
     model = load("naivebayes.joblib")
-    ret = model.predict([text])
-    return '#' + ret[0]
+    channel = model.predict([text])[0]
+    channel_id = findChannelId(channel)
+
+    return jsonify({
+	        "blocks": [
+		        {
+			        "type": "section",
+			        "text": {
+				        "type": "mrkdwn",
+				        "text": "I suggest your question, *"+text+"*, be posted in <#"+channel_id+"|"+channel+">."
+			        }
+		        }
+	        ]
+        })
+
+def findChannelId(channel_name):
+    try:
+        # Call the conversations.list method using the WebClient
+        for result in client.conversations_list(token = "xoxb-2303935478769-2297880316244-vKtkyqSkDMB6Ek6qeIGEJWtl"):
+            for channel in result["channels"]:
+                if channel["name"] == channel_name:
+                    return channel["id"]
+
+    except SlackApiError as e:
+        print(f"Error: {e}")
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
